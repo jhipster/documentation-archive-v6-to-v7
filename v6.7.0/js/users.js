43df8c849eda9cb131095f09d71cc8563752b49d
Sviatoslav Makhynko <sviat.mak@gmail.com>
2024-11-08 01:30:43 +0100

    add: content for archive

---
+++ v6.7.0/js/users.js
@@ -0,0 +1,24 @@
+(function () {
+    'use strict';
+    angular
+        .module('jhipster.users', [])
+        .config(['$interpolateProvider', function ($interpolateProvider) {
+            return $interpolateProvider.startSymbol('{(').endSymbol(')}');
+          }
+        ])
+        .controller('UsersCtrl', UsersCtrl);
+
+    UsersCtrl.$inject = ['$http', '$scope'];
+
+    function UsersCtrl($http, $scope) {
+        $scope.users = [];
+        $scope.showFeatured = true;
+        loadUsers();
+
+        function loadUsers() {
+            $http.get('/documentation-archive-v6-to-v7/v6.7.0/companies-using-jhipster/users.json').then(function(response) {
+                $scope.users = response.data.users;
+            });
+        }
+     }
+})();
---
+++ v7.9.3/js/users.js
@@ -0,0 +1,23 @@
+(function () {
+    'use strict';
+    angular
+        .module('jhipster.users', [])
+        .config(['$interpolateProvider', function ($interpolateProvider) {
+            return $interpolateProvider.startSymbol('{(').endSymbol(')}');
+        }])
+        .controller('UsersCtrl', UsersCtrl);
+
+    UsersCtrl.$inject = ['$http', '$scope'];
+
+    function UsersCtrl($http, $scope) {
+        $scope.users = [];
+        $scope.showFeatured = true;
+        loadUsers();
+
+        function loadUsers() {
+            $http.get('/documentation-archive-v6-to-v7/v7.9.3/companies-using-jhipster/users.json').then(function (response) {
+                $scope.users = response.data.users;
+            });
+        }
+    }
+})();
